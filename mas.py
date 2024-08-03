b4e7596da4d0e158a23159938be39429ff07ddbd
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2024-08-03 17:23:24 +0630

    Update mas.py

--- mas.py
+++ mas.py
@@ -7,6 +7,8 @@ import json
 node_url = 'http://node.masnet.ai:8545'
 web3 = Web3(Web3.HTTPProvider(node_url))
 
+Account.enable_unaudited_hdwallet_features()
+
 # Check if connected to the node
 if not web3.is_connected():
     raise Exception("Failed to connect to the Ethereum node")
